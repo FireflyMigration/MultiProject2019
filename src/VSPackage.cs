3d929fdb2a27b43e61715bb6bfa2537b279d48d0
Sefi <sefbar@gmail.com>
2019-04-23 20:48:25 +0300

    Initial commit

---
+++ src/VSPackage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace MultiVSIX2019
+{
+    [Guid("7222ad82-a34e-4c8e-ae3b-0a2714d5ffd8")]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    public sealed class VSPackage : AsyncPackage
+    {
+    }
+}
---
+++ src/source.extension.cs
@@ -0,0 +1,13 @@
+namespace MultiVSIX2019
+{
+    internal sealed partial class Vsix
+    {
+        public const string Id = "31ec69a9-5444-4299-a72f-9b35fc469fe3";
+        public const string Name = "Visual Studio Extension Bundle 2019";
+        public const string Description = "";
+        public const string Language = "en-US";
+        public const string Version = "1.5";
+        public const string Author = "Firefly";
+        public const string Tags = "extensions, vsix";
+    }
+}
